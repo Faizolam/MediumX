33c004ebfa6f3f50c3d6b82a2bdab0defdff8d54
FaizOlam <faizpupil@gmail.com>
2024-11-28 23:55:02 +0530

    Modify workflow to Test

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,9 +13,9 @@ from app.oauth2 import create_access_token
 # Debugging output for connection string
 print(f"Connecting to database {settings.DATABASE_NAME_TEST} at {settings.DATABASE_HOSTNAME}:{settings.DATABASE_PORT} with user {settings.DATABASE_USERNAME}")
 
-SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.DATABASE_USERNAME}:{settings.DATABASE_PASSWORD}@{settings.DATABASE_HOSTNAME}:{settings.DATABASE_PORT}/{settings.DATABASE_NAME_TEST}'
+SQLALCHEMY_DATABASE_URL_TEST = f'postgresql://{settings.DATABASE_USERNAME}:{settings.DATABASE_PASSWORD}@{settings.DATABASE_HOSTNAME}:{settings.DATABASE_PORT}/{settings.DATABASE_NAME_TEST}'
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL_TEST)
 
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
